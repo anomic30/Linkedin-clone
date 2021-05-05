c78944cbc59d9da6e34ecb3339216e243208a5b0
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2021-05-06 00:26:32 +0530

    changes

--- src/Widgets.js
+++ src/Widgets.js
@@ -15,7 +15,7 @@ function Widgets() {
                 <p>{subtitle}</p>
             </div>
         </div>
-    };
+    }
 
     return (
         <div className="widgets">
@@ -23,7 +23,40 @@ function Widgets() {
                 <h2>LinkedIn News</h2>
                 <InfoOutlinedIcon />
             </div>
-            {newsArticle("LinkedIn clone is finally released","Top news")}
+            
+            <div className="widgets__article">
+                <div className="separate">
+                    <div className="widgets__articleLeft">
+                        <FiberManualRecordIcon />
+                    </div>
+                    <div className="widgets__articleRight">
+                        <h4>Dogecoin</h4>
+                        <p>Finally reaches $1</p>
+                    </div>
+                </div>
+                <br/>
+                <div className="separate">
+                    <div className="widgets__articleLeft">
+                        <FiberManualRecordIcon />
+                    </div>
+                    <div className="widgets__articleRight">
+                        <h4>Tesla</h4>
+                        <p>Launches internet in India</p>
+                    </div>
+                </div>
+                <br />
+                <div className="separate">
+                    <div className="widgets__articleLeft">
+                        <FiberManualRecordIcon />
+                        
+                    </div>
+                    <div className="widgets__articleRight">
+                        <h4>SpaceX SN15</h4>
+                        <p>Launching soon!</p>
+                    </div>
+                </div>
+                <br/>
+            </div>
         </div>
     );
 
